aa0fc0872631c5fdc0a65ed2e4d7cb7c4c4219ac
Vadim Shkolenko <vadimand1998@gmail.com>
2021-02-05 16:54:45 +0300

    add solution for 03-streams 02-line-split-stream

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,12 +4,26 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.storage = '';
   }
 
   _transform(chunk, encoding, callback) {
+    this.storage += chunk.toString();
+    if (this.storage.includes(os.EOL)) {
+      const lines = this.storage.split(os.EOL);
+
+      this.storage = lines.pop();
+      lines.forEach((data) => this.push(data));
+    }
+    callback();
   }
 
   _flush(callback) {
+    if (this.storage) {
+      this.push(this.storage);
+    }
+
+    callback();
   }
 }
 
